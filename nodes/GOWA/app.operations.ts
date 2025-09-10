1e5803dd44706f7c3b6cbee0053cb9581878954e
“cielhaidir” <phoenicchi@gmail.com>
2025-09-10 22:36:54 +0800

    feat: add 'Get Media by Path' operation with media path validation and binary data handling

--- nodes/GOWA/app.operations.ts
+++ nodes/GOWA/app.operations.ts
@@ -25,6 +25,12 @@ export const appOperations: INodeProperties[] = [
 				action: 'Get device information',
 			},
 			{
+				name: 'Get Media by Path',
+				value: 'getMediaByPath',
+				description: 'Get media file by its storage path',
+				action: 'Get media file by storage path',
+			},
+			{
 				name: 'Reconnect',
 				value: 'reconnect',
 				description: 'Reconnect to WhatsApp Web',
@@ -32,6 +38,21 @@ export const appOperations: INodeProperties[] = [
 			},
 		],
 		default: 'getDeviceInfo',
+	},
+	{
+		displayName: 'Media Path',
+		name: 'mediaPath',
+		type: 'string',
+		required: true,
+		displayOptions: {
+			show: {
+				resource: ['app'],
+				operation: ['getMediaByPath'],
+			},
+		},
+		default: '',
+		placeholder: 'statics/media/628123456789/2024-01-15/1705123456-abc123.jpg',
+		description: 'The relative path to the media file (e.g., statics/media/phone/date/filename)',
 	},
 ];
 
@@ -55,11 +76,41 @@ export const executeAppOperation: OperationExecutor = async function (
 		case 'getDeviceInfo':
 			requestOptions.url = `${baseUrl.replace(/\/$/, '')}/app/devices`;
 			break;
+		case 'getMediaByPath':
+			const mediaPath = this.getNodeParameter('mediaPath', itemIndex) as string;
+			if (!mediaPath) {
+				throw new NodeOperationError(this.getNode(), 'Media path is required');
+			}
+			// Remove leading slash if present and ensure it starts with statics/
+			const cleanPath = mediaPath.replace(/^\/+/, '');
+			if (!cleanPath.startsWith('statics/')) {
+				throw new NodeOperationError(this.getNode(), 'Media path must start with "statics/"');
+			}
+			requestOptions.url = `${baseUrl.replace(/\/$/, '')}/${cleanPath}`;
+			break;
 		case 'reconnect':
 			requestOptions.url = `${baseUrl.replace(/\/$/, '')}/app/reconnect`;
 			break;
 		default:
 			throw new NodeOperationError(this.getNode(), `Unknown app operation: ${operation}`);
+	}
+
+	// For media operations, we need to handle binary data differently
+	if (operation === 'getMediaByPath') {
+		const response = await this.helpers.requestWithAuthentication.call(this, 'goWhatsappApi', {
+			...requestOptions,
+			encoding: 'binary',
+			returnFullResponse: true,
+		});
+		
+		return {
+			mediaPath: this.getNodeParameter('mediaPath', itemIndex),
+			statusCode: response.statusCode,
+			headers: response.headers,
+			contentType: response.headers['content-type'],
+			contentLength: response.headers['content-length'],
+			body: response.body,
+		};
 	}
 
 	const response = await this.helpers.requestWithAuthentication.call(this, 'goWhatsappApi', {
